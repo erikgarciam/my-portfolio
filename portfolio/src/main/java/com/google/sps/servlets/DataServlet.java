c4f5e266101b07ad833bffa81739bc3d80764f1a
Erik Garcia <erikgarciam@google.com>
2020-06-02 19:24:03 +0000

    Add getMessage() function - getFunction() recieves data from dataservlet.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,7 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
+    response.setContentType("text/html;"); 
     response.getWriter().println("<h1>Hello Erik !</h1>");
   }
 }
